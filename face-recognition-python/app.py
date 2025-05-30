97fd19bf263b821720ed2e58cd8fcb1ecc2569ef
Hung <ares01202975777@gmail.com>
2025-05-30 20:40:47 +0700

    fix to fit with api

--- face-recognition-python/app.py
+++ face-recognition-python/app.py
@@ -4,19 +4,20 @@ import numpy as np
 from PIL import Image
 import requests
 from io import BytesIO
+import json
 
 app = Flask(__name__)
 
-resp = requests.get("http://localhost:8080/api/face-encodings/all")
-data = resp.json()  # List of dicts: [{"id":1,"employeeId":"1", "encodingJson":"[...]"}, ...]
+resp = requests.get("http://localhost:8080/api/face-encodings")
+data = resp.json() 
 
-known_face_ids = [d['employeeId'] for d in data]
-known_face_encodings = [np.array(d['encodingJson'], dtype=np.float32) for d in data]
+known_face_ids = [d['employee']['id'] for d in data]
+known_face_encodings = [np.array(json.loads(d['encodingJson'])['encoding'], dtype=np.float32) for d in data]
 
 @app.route('/recognize', methods=['POST'])
 def recognize():
     file = request.files['image']
-    img = face_recognition.load_image_file(file)
+    img = face_recognition.load_image_file( file)
     faces = face_recognition.face_encodings(img)
 
     for face in faces:
@@ -49,10 +50,10 @@ def encode_face():
 
 @app.route('/get-encodings', methods=['GET'])
 def get_encodings():
-    resp = requests.get("http://localhost:8080/api/face-encodings/all")
+    resp = requests.get("http://localhost:8080/api/face-encodings")
     data = resp.json()  # List of dicts: [{"id":1,"employeeId":"1", "encodingJson":"[...]"}, ...]
     global known_face_ids, known_face_encodings
-    known_face_ids = [d['employeeId'] for d in data]
-    known_face_encodings = [np.array(d['encodingJson'], dtype=np.float32) for d in data]
+    known_face_ids = [d['employee']['id'] for d in data]
+    known_face_encodings = [np.array(json.loads(d['encodingJson'])['encoding'], dtype=np.float32) for d in data]
     
 app.run(debug=True)
